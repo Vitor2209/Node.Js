09c19b503432e0ab52b3e0d92c0844f2085d39d5
Vitor Dutra Melo <vitordutra1125@gmail.com>
2026-02-06 21:11:59 +0000

    feat: complete additional Node.js course exercise

--- noticias.js
+++ noticias.js
@@ -1,23 +1,5 @@
-var http = requere('http');
-
-var server = http.createServer(function(req, res){
-
-    var categoria = req.url;
-
-    if(categoria == '/tecnologia'){
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.end('<html><body>Noticias de tecnologia</body></html>');
-
-    } else if (categoria == '/moda') {
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.end('<html><body>Noticias de moda</body></html>');
-
-    } else {
-        res.writeHead(404, {"Content-Type": "text/html"});
-        res.end('<html><body>Página não encontrada</body></html>');
-    }
-
-    res.end('<html><body>Noticias de moda</body></html>');
-
-}).listen(3000);
-
+module.exports = function(app) {
+     application.get('/noticias', function(req, res) {
+        res.render('noticias/noticias');
+     });
+};
