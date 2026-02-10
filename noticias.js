9aca2080390eefd9196aa80228c9ff9b3bd77855
Vitor Dutra Melo <vitordutra1125@gmail.com>
2026-02-10 21:01:26 +0000

    feat: add class project

--- noticias.js
+++ noticias.js
@@ -1,5 +1,19 @@
-module.exports = function(app) {
-     application.get('/noticias', function(req, res) {
-        res.render('noticias/noticias');
-     });
-};
+module.exports = (app)  => {
+   var mysql = require('mysql');
+
+   var connection = mysql.createConnection({
+      host : 'localhost',
+      user : 'root',
+      password : 'root',
+      database : 'portal_noticias'
+   });
+
+
+   connection.query('select * from noticias', function(erro, result) {
+       res.send(result);
+      });
+
+      app.get('/noticias', function(req, res){
+         res.render('noticias/noticias', {noticias : result});
+      });
+}
